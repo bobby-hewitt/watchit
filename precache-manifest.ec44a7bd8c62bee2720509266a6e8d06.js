81a09ec5d118e5aa4179d44cbc55357cb09cd971
bobby <bobby.hewitt@w12studios.com>
2021-01-16 20:14:42 +0000

    Updates

---
+++ precache-manifest.ec44a7bd8c62bee2720509266a6e8d06.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "daaa0514f77443cae5e4145639df4853",
+    "url": "/watchit/index.html"
+  },
+  {
+    "revision": "a8964c2f0f0b3341213d",
+    "url": "/watchit/static/css/main.96aaa239.chunk.css"
+  },
+  {
+    "revision": "20ba1878bacb14065c9c",
+    "url": "/watchit/static/js/2.505dcbea.chunk.js"
+  },
+  {
+    "revision": "a8964c2f0f0b3341213d",
+    "url": "/watchit/static/js/main.85f3f996.chunk.js"
+  },
+  {
+    "revision": "09bfe36ef06ce83b935a",
+    "url": "/watchit/static/js/runtime-main.75b541aa.js"
+  },
+  {
+    "revision": "f878000cd1ac73eada4fcaeba4721af2",
+    "url": "/watchit/static/media/check.f878000c.svg"
+  },
+  {
+    "revision": "c49d092dde8369244a5db2f2d88e1537",
+    "url": "/watchit/static/media/chevrondown.c49d092d.svg"
+  },
+  {
+    "revision": "77fafd0e2b165f2099cbf0c68885d12f",
+    "url": "/watchit/static/media/chevronleft.77fafd0e.svg"
+  },
+  {
+    "revision": "9ce1d2ae04721fe6edd066d96fd63da2",
+    "url": "/watchit/static/media/chevronup.9ce1d2ae.svg"
+  },
+  {
+    "revision": "4d34a2f9b91828ef063117d13d68d885",
+    "url": "/watchit/static/media/edit.4d34a2f9.svg"
+  },
+  {
+    "revision": "356b0415774cd9290c4742fbc8cd173d",
+    "url": "/watchit/static/media/filter.356b0415.svg"
+  },
+  {
+    "revision": "081b7992ac98fde57b8a87dee3a2ad49",
+    "url": "/watchit/static/media/plus.081b7992.svg"
+  }
+]);
