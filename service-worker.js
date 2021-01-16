6822d4b6afa39255505163edae0767539efdf07a
bobby <bobby.hewitt@w12studios.com>
2021-01-16 23:59:32 +0000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/watchit/precache-manifest.bb892b2e55dd1b4c75667c090d089676.js"
+  "/watchit/precache-manifest.e178591393000bd5ed23c84bb670f3b7.js"
 );
 
 self.addEventListener('message', (event) => {
